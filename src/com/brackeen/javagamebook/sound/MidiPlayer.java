6f65510736be621e91cbee98ea91f67a3f5c0b48
Arthur Harper <harper10@purdue.edu>
2015-10-30 21:28:52 -0400

    fixed music

--- src/com/brackeen/javagamebook/sound/MidiPlayer.java
+++ src/com/brackeen/javagamebook/sound/MidiPlayer.java
@@ -93,6 +93,7 @@ public class MidiPlayer implements MetaEventListener {
     public void meta(MetaMessage event) {
         if (event.getType() == END_OF_TRACK_MESSAGE) {
             if (sequencer != null && sequencer.isOpen() && loop) {
+                sequencer.setTickPosition(0);
                 sequencer.start();
             }
         }
